810e8c9ea43d9e08f760015d3010a2491ad1ce0a
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-30 00:52:03 +1300

    :art: Chore: go format

--- advanced-go/linked-list/main.go
+++ advanced-go/linked-list/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	linkedList = doubly.NewLinkedList()
+	ll = doubly.NewLinkedList()
 )
 
 func main() {
@@ -18,84 +18,84 @@ func main() {
 
 func examplesOne() {
 	fmt.Println("pushing 32")
-	linkedList.Push(32)
-	linkedList.Print()
+	ll.Push(32)
+	ll.Print()
 
 	fmt.Println("pushing 10")
-	linkedList.Push(10)
-	linkedList.Print()
+	ll.Push(10)
+	ll.Print()
 
 	fmt.Println("pushing 5")
-	linkedList.Push(5)
-	linkedList.Print()
+	ll.Push(5)
+	ll.Print()
 
 	fmt.Println("popping")
-	linkedList.Pop()
-	linkedList.Print()
+	ll.Pop()
+	ll.Print()
 
 	fmt.Println("popping")
-	linkedList.Pop()
-	linkedList.Print()
+	ll.Pop()
+	ll.Print()
 
 	fmt.Println("popping")
-	linkedList.Pop()
-	linkedList.Print()
+	ll.Pop()
+	ll.Print()
 
 	fmt.Println("populating linked list...")
-	linkedList.Push(1)
-	linkedList.Push(290)
-	linkedList.Push(948)
-	linkedList.Push(1920)
-	linkedList.Push(100000)
-	linkedList.Push(2)
-	linkedList.Print()
+	ll.Push(1)
+	ll.Push(290)
+	ll.Push(948)
+	ll.Push(1920)
+	ll.Push(100000)
+	ll.Push(2)
+	ll.Print()
 
 	fmt.Println("unshifting 88...")
-	linkedList.Unshift(88)
-	linkedList.Print()
+	ll.Unshift(88)
+	ll.Print()
 
 	fmt.Println("searching value 1920...")
-	linkedList.FindWithPrint(1920)
+	ll.FindWithPrint(1920)
 
 	fmt.Println("searching value 5...")
-	linkedList.FindWithPrint(5)
+	ll.FindWithPrint(5)
 
 	fmt.Println("shifting...")
-	linkedList.Shift()
-	linkedList.Print()
+	ll.Shift()
+	ll.Print()
 
 	fmt.Println("shifting...")
-	linkedList.Shift()
-	linkedList.Print()
+	ll.Shift()
+	ll.Print()
 
 	fmt.Println("shifting...")
-	linkedList.Shift()
-	linkedList.Print()
+	ll.Shift()
+	ll.Print()
 
 	fmt.Println("inserting at 2 value 19...")
-	linkedList.Insert(19, 2)
-	linkedList.Print()
+	ll.Insert(19, 2)
+	ll.Print()
 
 	fmt.Println("inserting at 1 value 33...")
-	linkedList.Insert(33, 1)
-	linkedList.Print()
+	ll.Insert(33, 1)
+	ll.Print()
 
 	fmt.Println("inserting at 99 value 999...")
-	linkedList.Insert(999, 99)
-	linkedList.Print()
+	ll.Insert(999, 99)
+	ll.Print()
 }
 
 func examplesTwo() {
 	fmt.Println("populating linked list...")
-	linkedList.Push(1)
-	linkedList.Push(2)
-	linkedList.Push(3)
-	linkedList.Push(4)
-	linkedList.Push(5)
-	linkedList.Push(6)
-	linkedList.Print()
+	ll.Push(1)
+	ll.Push(2)
+	ll.Push(3)
+	ll.Push(4)
+	ll.Push(5)
+	ll.Push(6)
+	ll.Print()
 
-	n, ok := linkedList.Find(1)
+	n, ok := ll.Find(1)
 	if ok {
 		n.Print()
 	}
