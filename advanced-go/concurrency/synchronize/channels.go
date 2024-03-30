9dbeed1d1740d2697268033a81c87ec3be3beab6
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-30 14:43:34 +1300

    :fire: Feat: remove wait groups from channels usacase

--- advanced-go/concurrency/synchronize/channels.go
+++ advanced-go/concurrency/synchronize/channels.go
@@ -2,7 +2,6 @@ package synchronize
 
 import (
 	"fmt"
-	"sync"
 )
 
 func UsingChannels(countTo int) {
@@ -12,33 +11,25 @@ func UsingChannels(countTo int) {
 	ch := make(chan *int, cBuffer)
 	defer close(ch)
 
-	wg := sync.WaitGroup{}
-
 	counter := 0
 
-	for i := 1; i <= countTo; i++ {
+	for i := 0; i <= countTo; i++ {
 		// NOTE: the channel will block/wait until a new item can be added (max buffer per time)
-		// 			 a.k.a: as soon as another thread releases/consumes an item from the channel
+		// 			 a.k.a: as soon as another goroutine releases/consumes an item from the channel
 		ch <- &counter
 
-		wg.Add(1)
-
-		go printChannel(&wg, ch)
+		go printChannel(ch)
 	}
-
-	wg.Wait()
 
 	fmt.Printf("total counter: %v\n", counter)
 }
 
-func printChannel(wg *sync.WaitGroup, ch chan *int) {
-	defer wg.Done()
-
+func printChannel(ch chan *int) {
 	// NOTE: consuming/releasing an item from the channel
-	// 			 allowing to another thread add a new item to it
+	// 			 allowing to another goroutines add a new item to it
 	counter := <-ch
 
-	// NOTE: as this channel has a buffer of one, only one thread per time will
+	// NOTE: as this channel has a buffer of one, only one goroutines per time will
 	// 			 acquire the channel and change the counter
 	*counter++
 
