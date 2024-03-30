a6cfd9b8768bf7a9c122d4b439c70ddc593d368a
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-30 17:01:09 +1300

    :test_tube: Tests: linked list method push

--- advanced-go/structures/linkedlist/doubly/push_test.go
+++ advanced-go/structures/linkedlist/doubly/push_test.go
@@ -3,18 +3,23 @@ package doubly
 import (
 	"testing"
 
+	"github.com/lucasfalm/go-training/advanced-go/structures/linkedlist/node"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPush(t *testing.T) {
 	t.Run("a string object", func(t *testing.T) {
-		dll.Push("string")
+		assert.Equal(t, dll.Any(), false)
+
+		assert.Implements(t, (*node.NodeInterface)(nil), dll.Push("string"))
 
 		assert.Equal(t, dll.Any(), true)
 	})
 
 	t.Run("a nil object", func(t *testing.T) {
-		dll.Push(nil)
+		assert.Equal(t, dll.Any(), false)
+
+		assert.Implements(t, (*node.NodeInterface)(nil), dll.Push(nil))
 
 		assert.Equal(t, dll.Any(), true)
 	})
