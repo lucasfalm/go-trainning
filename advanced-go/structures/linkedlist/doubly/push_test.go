98d239474872c38a80553f3fe57fa248128ce5d4
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-30 17:16:41 +1300

    :test_tube: Tests: more unit tests to the push method from doubly linked lists

--- advanced-go/structures/linkedlist/doubly/push_test.go
+++ advanced-go/structures/linkedlist/doubly/push_test.go
@@ -9,18 +9,73 @@ import (
 
 func TestPush(t *testing.T) {
 	t.Run("a string object", func(t *testing.T) {
-		assert.Equal(t, dll.Any(), false)
+		assert.Equal(t, false, dll.Any())
 
-		assert.Implements(t, (*node.NodeInterface)(nil), dll.Push("string"))
+		result := dll.Push("string")
 
-		assert.Equal(t, dll.Any(), true)
+		assert.Implements(t, (*node.NodeInterface)(nil), result)
+
+		assert.Equal(t, true, dll.Any())
+
+		assert.Equal(t, nil, result.Head())
+		assert.Equal(t, nil, result.Tail())
+		assert.Equal(t, "string", result.Value())
 	})
 
 	t.Run("a nil object", func(t *testing.T) {
-		assert.Equal(t, dll.Any(), false)
+		assert.Equal(t, false, dll.Any())
 
-		assert.Implements(t, (*node.NodeInterface)(nil), dll.Push(nil))
+		result := dll.Push(nil)
 
-		assert.Equal(t, dll.Any(), true)
+		assert.Implements(t, (*node.NodeInterface)(nil), result)
+
+		assert.Equal(t, true, dll.Any())
+
+		assert.Equal(t, nil, result.Head())
+		assert.Equal(t, nil, result.Tail())
+		assert.Equal(t, nil, result.Value())
+	})
+
+	t.Run("multiple pushes", func(t *testing.T) {
+		assert.Equal(t, false, dll.Any())
+
+		// NOTE: first push
+		resultOne := dll.Push(1)
+
+		assert.Implements(t, (*node.NodeInterface)(nil), resultOne)
+
+		assert.Equal(t, true, dll.Any())
+
+		assert.Equal(t, nil, resultOne.Head())
+
+		assert.Equal(t, nil, resultOne.Tail())
+
+		assert.Equal(t, 1, resultOne.Value())
+
+		// NOTE: second push
+		resultTwo := dll.Push(2)
+
+		assert.Implements(t, (*node.NodeInterface)(nil), resultTwo)
+
+		assert.Equal(t, resultOne, resultTwo.Head())
+
+		assert.Equal(t, resultTwo, resultOne.Tail())
+
+		assert.Equal(t, nil, resultTwo.Tail())
+
+		assert.Equal(t, 2, resultTwo.Value())
+
+		// NOTE: third push
+		resultThree := dll.Push(3)
+
+		assert.Implements(t, (*node.NodeInterface)(nil), resultThree)
+
+		assert.Equal(t, resultTwo, resultThree.Head())
+
+		assert.Equal(t, nil, resultThree.Tail())
+
+		assert.Equal(t, resultThree, resultTwo.Tail())
+
+		assert.Equal(t, 3, resultThree.Value())
 	})
 }
