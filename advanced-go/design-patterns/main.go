5c744718a2125265c0ca05f0523c19b39ce66a73
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-31 23:23:04 +1300

    :sparkles: Feat: observer design pattern

--- advanced-go/design-patterns/main.go
+++ advanced-go/design-patterns/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 
 	"github.com/lucasfalm/go-training/advanced-go/design-patterns/builder"
 	"github.com/lucasfalm/go-training/advanced-go/design-patterns/decorator"
+	"github.com/lucasfalm/go-training/advanced-go/design-patterns/observer"
 )
 
 func main() {
+	showObserver()
+
 	// showDecorator()
-	showBuilder()
+
+	// showBuilder()
+
 }
 
 func showBuilder() {
@@ -52,4 +60,72 @@ func showDecorator() {
 	foodTwo = &decorator.Sweet{Food: foodTwo}
 
 	fmt.Println(foodTwo.Description(), foodTwo.Price())
+}
+
+var (
+	MyFuncSubject = observer.EventSubject{Observers: sync.Map{}}
+)
+
+func showObserver() {
+	var (
+		observerOne = observer.EventObserver{ID: 1, Time: time.Now()}
+		observerTwo = observer.EventObserver{ID: 2, Time: time.Now()}
+	)
+
+	MyFuncSubject.AddListener(&observerOne)
+	MyFuncSubject.AddListener(&observerTwo)
+
+	one, two, three := something(1, 2, 3)
+	fmt.Println(one, two, three)
+
+	switch one {
+	case 1002:
+		fmt.Println("1002")
+		fallthrough
+	default:
+		fmt.Println("will also pass here")
+	}
+
+	c := make(chan bool)
+
+	go usingChannels(c)
+
+	fmt.Println("before goroutine")
+
+	c <- true
+
+	// NOTE: the same goroutine cannot send and receive, so it will
+	// 			 block here until another goroutine sends a signal back
+	<-c
+}
+
+func something(a, _b, _c int) (one, two, three int) {
+	event := observer.Event{Data: observer.DataSchema{Title: "hey you", Body: "wanna have some fun?"}}
+	MyFuncSubject.Notify(event)
+
+	one = a + 1
+	changingValues(&one, 1000)
+	two = one + 2
+	three = *(multiplier())
+	return
+}
+
+func multiplier() (number *int) {
+	i := rand.Intn(100*int(time.Millisecond)) / 100000
+	number = &i
+	return
+}
+
+func changingValues(value *int, newValue int) {
+	*value = newValue + *value
+	return
+}
+
+func usingChannels(c chan bool) {
+	if <-c { // it get the value from the channel
+		time.Sleep(3 * time.Second)
+		fmt.Println("it was true")
+	}
+
+	c <- true // sends a signal back and releases the showObserver function
 }
