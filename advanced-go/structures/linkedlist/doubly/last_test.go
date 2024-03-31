ba8e39a2cbe197f146c14eab156404638e52ff49
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-31 13:37:17 +1300

    :test_tube: Tests: linked list last method

---
+++ advanced-go/structures/linkedlist/doubly/last_test.go
@@ -0,0 +1,28 @@
+package doubly
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLast(t *testing.T) {
+	t.Run("when there is no node", func(t *testing.T) {
+		setup()
+
+		result := dll.Last()
+
+		assert.Nil(t, result)
+	})
+
+	t.Run("when there is at least one node", func(t *testing.T) {
+		setup()
+
+		dll.Push("hello")
+
+		result := dll.Last()
+
+		assert.NotNil(t, result)
+		assert.Equal(t, "hello", result.Value())
+	})
+}
