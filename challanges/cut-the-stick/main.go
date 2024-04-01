a40e94990b7a0e89094e8a8841a069a331fe7a62
Lucas Feijo <feijolucas1997@gmail.com>
2024-04-01 15:41:50 +1300

    :sparkles: Feat: cut the sticks challenge

---
+++ challanges/cut-the-stick/main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int32{8, 6, 5, 3, 2, 2}
+
+	fmt.Println(arr)
+	fmt.Printf("cut: %v\n", cutTheSticks(arr))
+}
+
+// add the current length
+// find the shortest
+// decrement each by the shortest (including the shortest)
+//
+//	P1, P2 pointers
+//
+// 	P1 shortest number for the round (entire loop)
+//  P2 cuts each by the shortest (entire loop)
+// 	add length of final loop result
+//  repeat until no number left
+
+// NOTE: https://www.hackerrank.com/challenges/cut-the-sticks/problem?isFullScreen=true
+func cutTheSticks(arr []int32) []int32 {
+	result := []int32{}
+
+	// NOTE: pointers
+	p1, p2 := 0, 0
+
+	// NOTE: min number for the round
+	min := int32(0)
+
+	result = append(result, int32(len(arr)))
+
+	// NOTE: while there is a number
+	for len(arr) > 0 {
+		// NOTE: move P1 and find min
+		if p1 <= len(arr)-1 {
+			if min == 0 || min > arr[p1] {
+				min = arr[p1]
+			}
+
+			p1++
+		}
+
+		// NOTE: when P1 cannot move, then move P2 and cut sticks
+		if p1 > len(arr)-1 {
+
+			// NOTE: when P2 can move
+			if p2 <= len(arr)-1 {
+
+				arr[p2] -= min
+
+				if len(arr) == 1 {
+					arr = []int32{}
+				}
+
+				if len(arr) > 0 && arr[p2] <= 0 {
+					arr = append(arr[:p2], arr[p2+1:]...)
+				} else {
+					p2++
+				}
+			}
+
+			// NOTE: when P2 cannot move or only one number left
+			if len(arr) > 0 && (p2 > len(arr)-1 || len(arr) == 1) {
+				result = append(result, int32(len(arr)))
+
+				p1 = 0
+				p2 = 0
+				min = 0
+			}
+		}
+	}
+
+	return result
+}
